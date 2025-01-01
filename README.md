56bfba7e76df096529ec53f2f38b748bd4ec1363
Josh Finnis <josh.finnis@bunchball.com>
2025-01-01 13:14:45 -0500

    feat: add ability to execute languages in code blocks

--- README.md
+++ README.md
@@ -2,10 +2,39 @@
 
 This is a plugin for presenting markdown files, following along teej_dv's video series.
 
-# Usage
+# Features
+
+Can execute code in lua blocks when you have them in a slide.
 
 ```lua
-require('present').start_presentation {}
+print('Hello world', 37)
+```
+
+# Features
+
+Can execute code in javascript blocks when you have them in a slide.
+
+```javascript
+console.log({ myField: true, other: false })
+```
+
+# Features: Other languages
+
+Can execute code in language blocks when you have them in a slide.
+
+You can configure this with `opts.executors`, Python and Javascript work by
+default.
+
+```python
+print('this is python')
+```
+
+# Usage
+
+```
+:PresentStart
 ```
 
 Use `n` and `p` to navigate markdown slides.
+
+`X` will execute the code block on the page.
--- lua/present.lua
+++ lua/present.lua
@@ -1,5 +1,4 @@
-print 'hello from present.nvim'
-    -- TODO: get lsp working with the global vim api
+-- TODO: get lsp working with the global vim api
 
 local M = { }
 
@@ -7,14 +6,73 @@ local function create_floating_window(config, enter)
     if enter == nil then
         enter = false
     end
+
     local buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
     local win = vim.api.nvim_open_win(buf, enter, config)
 
     return { buf = buf, win = win }
 end
 
-M.setup = function()
-    -- nothing
+--- Default executor for lua code
+---@param block present.Block
+local execute_lua_code = function(block)
+    -- Override the default print function to capture all of the output.
+    -- Store the original print function.
+    local original_print = print
+
+    local output = {}
+
+    -- Redefine the print function
+    print = function(...)
+        local args = { ... }
+        local message = table.concat(vim.tbl_map(tostring, args), '\t')
+        table.insert(output, message)
+    end
+
+    -- Call the provided function
+    local chunk = loadstring(block.body)
+    pcall(function()
+        if not chunk then
+            table.insert(output, ' <<<BROKEN CODE>>>')
+        else
+            chunk()
+        end
+
+        return output
+    end)
+
+    -- Restore the original print function
+    print = original_print
+
+    return output
+end
+
+M.create_system_executor = function(program)
+    return function(block)
+        local tempfile = vim.fn.tempname()
+        vim.fn.writefile(vim.split(block.body, '\n'), tempfile)
+        local result = vim.system({ program, tempfile }, { text = true }):wait()
+        return vim.split(result.stdout, '\n')
+    end
+end
+
+local options = {
+    executors = {
+        lua = execute_lua_code,
+        javascript = M.create_system_executor 'node',
+        python = M.create_system_executor 'python3',
+    }
+}
+
+M.setup = function(opts)
+    opts = opts or {}
+    opts.executors = opts.executors or {}
+
+    opts.executors.lua = opts.executors.lua or execute_lua_code
+    opts.executors.javascript = opts.executors.javascript or M.create_system_executor 'node'
+    opts.executors.python = opts.executors.python or M.create_system_executor 'python'
+
+    options = opts
 end
 
 ---@class present.Slides
@@ -23,6 +81,11 @@ end
 ---@class present.Slide
 ---@field title string: The title of the slide
 ---@field body string[]: The body of the slide
+---@field block present.Block[]: A codeblock inside of a slide
+
+---@class present.Block
+---@field language string: The language of the code block
+---@field body string: The body of the code block
 
 --- Takes some lines and parses them
 ---@param lines string[]: The lines in the buffer
@@ -31,7 +94,8 @@ local parse_slides = function(lines)
     local slides = { slides = { } }
     local current_slide = {
         title = '',
-        body = {}
+        body = {},
+        blocks = {}
     }
 
     local separator = '^#'
@@ -45,7 +109,8 @@ local parse_slides = function(lines)
 
             current_slide = {
                 title = line,
-                body = {}
+                body = {},
+                blocks = {}
             }
         else
             table.insert(current_slide.body, line)
@@ -53,6 +118,32 @@ local parse_slides = function(lines)
     end
 
     table.insert(slides.slides, current_slide)
+
+    -- Iterate over blocks
+    for _, slide in ipairs(slides.slides) do
+        local block = {
+            language = nil,
+            body = '',
+        }
+        local inside_block = false
+        for _, line in ipairs(slide.body) do
+            if vim.startswith(line, '```') then
+                if not inside_block then
+                    inside_block = true
+                    block.language = string.sub(line, 4)
+                else
+                    inside_block = false
+                    block.body = vim.trim(block.body)
+                    table.insert(slide.blocks, block)
+                end
+            else
+                -- Inside a current markdown block but it is not one of the guards so insert text.
+                if inside_block then
+                    block.body = block.body .. line .. '\n'
+                end
+            end
+        end
+    end
 
     return slides
 end
@@ -176,6 +267,52 @@ M.start_presentation = function(opts)
         vim.api.nvim_win_close(state.floats.body.win, true)
     end)
 
+    present_keymap('n', 'X', function()
+        local slide = state.parsed.slides[state.current_slide]
+        -- TODO: make a way for people to execute this for other languages
+        local block = slide.blocks[1]
+        if not block then
+            print('No blocks on this page')
+            return
+        end
+
+        local executor = options.executors[block.language]
+        if not executor then
+            print('No valid executor for this language')
+            return
+        end
+
+        -- Table to capture print messages
+        local output = { '# Code', '', '```' .. block.language }
+        vim.list_extend(output, vim.split(block.body, '\n'))
+        table.insert(output, '```')
+
+        table.insert(output, '')
+        table.insert(output, '# Output')
+        table.insert(output, '')
+        table.insert(output, '```')
+        vim.list_extend(output, executor(block))
+        table.insert(output, '```')
+
+        -- Open a window to show print output
+        local buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+        local temp_width = math.floor(vim.o.columns * 0.8)
+        local temp_height = math.floor(vim.o.lines * 0.8)
+        vim.api.nvim_open_win(buf, true, {
+            relative = 'editor',
+            style = 'minimal',
+            noautocmd = true,
+            width = temp_width,
+            height = temp_height,
+            row = math.floor((vim.o.lines - temp_height) / 2),
+            col = math.floor((vim.o.columns - temp_width) / 2),
+            border = 'rounded'
+        })
+
+        vim.bo[buf].filetype = 'markdown'
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
+    end)
+
     -- We want to set some settings to make the presentation look nice, but we don't want those
     -- settings to globally override the user's settings. We can use an autocmd to restore them.
     local restore = {
@@ -224,8 +361,6 @@ M.start_presentation = function(opts)
 
     set_slide_content(state.current_slide)
 end
-
--- M.start_presentation { bufnr = 13 }
 
 -- Expose the parse_slides method for testing
 M._parse_slides = parse_slides
